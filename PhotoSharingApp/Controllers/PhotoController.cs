bb37fe6950bbb5e6d24dd2c003544e55a7ba0e5f
unknown <mustapha.hajromdhane@etudiant-enit.utm.tn>
2018-05-15 17:10:24 +0100

    1

--- PhotoSharingApp/Controllers/PhotoController.cs
+++ PhotoSharingApp/Controllers/PhotoController.cs
@@ -1,16 +1,18 @@
-﻿using System;
+﻿using PhotoSharingApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace PhotoSharingApp.Controller
+namespace PhotoSharingApp.Controllers
 {
     public class PhotoController : System.Web.Mvc.Controller
     {   // GET: Photo
         public ActionResult Index()
         {
-            return View();
+            Photo photo = new Photo(); 
+            return View("Index", photo);
         }
     }
 }
